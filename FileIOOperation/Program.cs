20f379f7ed6002e0b20d38b529eccc5b28d79f05
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-01-21 19:07:30 +0530

    JSONSerialization Added

--- FileIOOperation/Program.cs
+++ FileIOOperation/Program.cs
@@ -11,17 +11,26 @@ namespace FileIOOperation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Serialization and DeSerialazation..!");
-            Console.WriteLine("\n1. Binary Serialization. \t2. Binary DeSerialization");
+            Console.WriteLine("\n1. Binary Serialization. \n2. Binary DeSerialization");
+            Console.WriteLine("3. JSON Serialization. \n4. JSON DeSerialization");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
-                //Binary Serialization Operation
+                    //Binary Serialization Operation
                 case 1:
                     SerializatioOpearion.BinarySerializationFn();
                     break;
-                //Binary DeSerialization Operation
+                    //Binary DeSerialization Operation
                 case 2:
                     SerializatioOpearion.BinaryDeSerializationFn();
+                    break;
+                case 3:
+                    //JSON Serialization Operation
+                    SerializatioOpearion.JSONSerializationFn();
+                break;
+                case 4:
+                    //JSON DeSerialization Operation
+                    SerializatioOpearion.JSONDeSerializationFn();
                     break;
                 default:
                     Console.WriteLine("Choose from given option Only");
--- FileIOOperation/SerializatioOpearion.cs
+++ FileIOOperation/SerializatioOpearion.cs
@@ -1,44 +1,104 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileIOOperation
 {
     public class SerializatioOpearion
     {
-        public static string path = @"C:\Users\hksol\source\repos\FileIOOperation\FileIOOperation\Files\BinaryData.txt";
         //Binary Serialization Operation
         public static void BinarySerializationFn()
         {
-            List<Contact> contact = new List<Contact>()
+            string path = @"C:\Users\hksol\source\repos\FileIOOperation\FileIOOperation\Files\BinaryData.txt";
+            try
             {
-                new Contact() {Id = 363, Name="Hitesh", Age= 26},
-                new Contact() {Id = 364, Name="Dheeraj", Age= 24},
-                new Contact() {Id = 365, Name="Surya", Age= 28},
-            };
-            FileStream stream = new FileStream(path,FileMode.OpenOrCreate);
-            BinaryFormatter bf = new BinaryFormatter();
-            bf.Serialize(stream, contact);
-            stream.Close();
-            Console.WriteLine("Binary Serialization Completed Succesfully");
+                List<Contact> contact = new List<Contact>()
+                {
+                    new Contact() {Id = 363, Name="Hitesh", Age= 26},
+                    new Contact() {Id = 364, Name="Dheeraj", Age= 24},
+                    new Contact() {Id = 365, Name="Surya", Age= 28},
+                };
+                FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(stream, contact);
+                stream.Close();
+                Console.WriteLine("Binary Serialization Completed Succesfully");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
         //Binary DeSerialization Operation
         public static void BinaryDeSerializationFn()
         {
-           
-            FileStream stream = new FileStream(path, FileMode.Open);
-            BinaryFormatter bf = new BinaryFormatter();
-            List<Contact> listofcontact =(List<Contact>) bf.Deserialize(stream);
-            stream.Close();
-            foreach(Contact contact in listofcontact)
+            string path = @"C:\Users\hksol\source\repos\FileIOOperation\FileIOOperation\Files\BinaryData.txt";
+            try
             {
-                Console.WriteLine(contact);
+                FileStream stream = new FileStream(path, FileMode.Open);
+                BinaryFormatter bf = new BinaryFormatter();
+                List<Contact> listofcontact = (List<Contact>)bf.Deserialize(stream);
+                stream.Close();
+                foreach (Contact contact in listofcontact)
+                {
+                    Console.WriteLine(contact);
+                }
+                Console.WriteLine("Binary Deserialization Completed Succesfully");
             }
-            Console.WriteLine("Binary Deserialization Completed Succesfully");
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //JSON Serialization Operation
+        public static void JSONSerializationFn()
+        {
+            string path = @"C:\Users\hksol\source\repos\FileIOOperation\FileIOOperation\Files\JSONData.json";
+            try
+            {
+                List<Contact> contact = new List<Contact>()
+                {
+                    new Contact() {Id = 363, Name="Manish", Age= 26},
+                    new Contact() {Id = 364, Name="Dheeraj", Age= 24},
+                    new Contact() {Id = 365, Name="Ramesh", Age= 28},
+                };
+                string jsonData = JsonConvert.SerializeObject(contact);
+                File.WriteAllText(path, jsonData);
+                Console.WriteLine("JSON Serialization Completed Succesfully");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //JSON DeSerialization Operation
+        public static void JSONDeSerializationFn()
+        {
+            string path = @"C:\Users\hksol\source\repos\FileIOOperation\FileIOOperation\Files\JSONData.json";
+            try
+            {
+                string jsonData = File.ReadAllText(path);
+                List<Contact> contacts = JsonConvert.DeserializeObject<List<Contact>>(jsonData);
+                foreach (Contact contact in contacts)
+                {
+                    Console.WriteLine(contact);
+                }
+                Console.WriteLine("JSON Deserialization Completed Succesfully");
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
